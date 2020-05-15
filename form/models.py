3dd5b3dafdee44e7b0a100e8502936d43cff4b83
elvis-gene <elvis.pgene@gmail.com>
2020-05-15 05:35:07 +0200

    form update - on city field next

--- form/models.py
+++ form/models.py
@@ -5,5 +5,19 @@ class Student(models.Model):
     surname = models.CharField(max_length=50)
     institution = models.CharField(max_length=100)
     city = models.CharField(max_length=50)
-    visa_expiry_date = models.DateTimeField('expiry date')
+    visa_expiry_date = models.DateField('expiry date')
     total = models.IntegerField()
+
+
+# No need to use this. Will use the Django built-in function
+    # def __init__(self, first_name, surname, institution, city, visa_expiry_date):
+    #     models.Model.__init__(self)
+    #     self.first_name = first_name
+    #     self.surname = surname
+    #     self.institution = institution
+    #     self.city = city
+    #     self.visa_expiry_date = visa_expiry_date
+
+
+    def __str__(self):
+        return '{} {}'.format(self.surname, self.first_name)
--- form/views.py
+++ form/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from .models import Student
 from django.http import HttpResponse
+from django.forms import modelform_factory
+from .models import Student
 
 
+StudentForm = modelform_factory(Student, exclude=['total'])
 def home(request):
-    return render(request, 'form/index.html')
+    form = StudentForm()
+    return render(request, 'form/index.html', {'form': form})
 
 
 def process_form(request):
     return HttpResponse("Merci pour l'enregistrement")
+
+
+# Another way to replace the two methods above is to use a 'CreateView'
