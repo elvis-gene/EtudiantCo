89f01f40c1645c24ab36cde0c8df56f3377824bd
elvis-gene <elvis.pgene@gmail.com>
2020-05-17 19:06:27 +0200

    Form Completed

--- form/models.py
+++ form/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
+# This will allow me to count the number of students
+students_count = 0
+
 # Could also use a CharField and provide the options in the HTML.
 provinces_list = (
     ('1', 'Eastern Cape'),
@@ -14,11 +17,12 @@ provinces_list = (
     ('9', 'Western Cape'),
 )
 
+# Add help texts
 class Student(models.Model):
     first_name = models.CharField(max_length=50, help_text='Veuillez entrer votre numero de telephone.')
     surname = models.CharField(max_length=50)
     email = models.EmailField(max_length=254)
-    phone_number_country_code = PhoneNumberField()
+    # phone_number_country_code = PhoneNumberField()
     phone_number = PhoneNumberField()
     address = models.CharField(max_length=200)
     city = models.CharField(max_length=50)
@@ -26,18 +30,7 @@ class Student(models.Model):
     zip_code = models.IntegerField()
     institution = models.CharField(max_length=100)
     visa_expiry_date = models.DateField('expiry date')
-    total = models.IntegerField()
 
 
     def __str__(self):
         return '{} {}'.format(self.surname, self.first_name)
-
-
-# No need to use this. Will use the Django built-in function
-    # def __init__(self, first_name, surname, institution, city, visa_expiry_date):
-    #     models.Model.__init__(self)
-    #     self.first_name = first_name
-    #     self.surname = surname
-    #     self.institution = institution
-    #     self.city = city
-    #     self.visa_expiry_date = visa_expiry_date
--- form/views.py
+++ form/views.py
@@ -3,15 +3,16 @@ from .models import Student
 from django.http import HttpResponse
 from django.forms import modelform_factory
 from .models import Student
-from phonenumber_field.widgets import PhoneNumberPrefixWidget, PhonePrefixSelect
+from phonenumber_field.widgets import PhoneNumberPrefixWidget
 from django import forms
 
 
 # Use SelectateWidget
- # 'phone_number': PhoneNumberPrefixWidget(initial='ZA'),
+# 'phone_number_country_code': PhonePrefixSelect(initial='ZA'),
 StudentForm = modelform_factory(Student, exclude=['total'],
-    widgets={'phone_number_country_code': PhonePrefixSelect(initial='ZA'),
-             'province': forms.Select})
+    widgets={'phone_number': PhoneNumberPrefixWidget(attrs={'pattern': '[0-9]+'}, initial='ZA'),
+             'province': forms.Select(),
+             'visa_expiry_date': forms.SelectDateWidget()})
 
 def home(request):
     form = StudentForm()
@@ -23,3 +24,4 @@ def process_form(request):
 
 
 # Another way to replace the two methods above is to use a 'CreateView'
+# Later not to allow someone to input date in the past
